8a6d3fbc7fad5d19420ae252bc9ba23f7980d107
Janny0222 <villarueljanmichael@gmail.com>
2025-09-11 13:43:15 +0800

    add registration

--- src/components/ui/loader/BeatLoader.jsx
+++ src/components/ui/loader/BeatLoader.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import MainModal from "../../modals/MainModal";
 import { BeatLoader } from "react-spinners";
 
